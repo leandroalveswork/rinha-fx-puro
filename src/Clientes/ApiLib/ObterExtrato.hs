fb8c44f5c28b51ce07d0e43bc070b9262dcbaefb
leandroalveswork <leandroprogramadoruxdesign@gmail.com>
2024-04-17 22:05:22 -0300

    usar a biblioteca de postgres e implementar endpoint GET

--- src/Clientes/ApiLib/ObterExtrato.hs
+++ src/Clientes/ApiLib/ObterExtrato.hs
@@ -6,40 +6,44 @@ module Clientes.ApiLib.ObterExtrato
   ( obterExtrato
   ) where
 
-import Data.ByteString                         (ByteString)
+import Data.ByteString                          (ByteString)
 import Control.Concurrent
-import Control.Exception                       (bracket)
+import Control.Exception                        (bracket)
 import Control.Monad.IO.Class
 import Data.Pool
 import Database.PostgreSQL.Simple
-import Network.HTTP.Client                     (newManager, defaultManagerSettings)
+import Network.HTTP.Client                      (newManager, defaultManagerSettings)
 import Network.Wai.Handler.Warp
 import Servant
 import Servant.Client
-import Data.Time                               (UTCTime)
-import Clientes.ViewModelsLib.InputTransacaoVM (InputTransacao)
-import Clientes.ViewModelsLib.SaldoVM          (Saldo)
-import Clientes.ViewModelsLib.ExtratoNaHoraVM  (ExtratoNaHora)
+import Data.Time                                (UTCTime)
+import Clientes.ViewModelsLib.InputTransacaoVM  (InputTransacao)
+import Clientes.ViewModelsLib.SaldoVM           (Saldo)
+import Clientes.ViewModelsLib.SaldoNaHoraVM     (SaldoNaHora)
+import Clientes.ViewModelsLib.ExtratoNaHoraVM   (ExtratoNaHora, saldo, ultimas_transacoes)
+import Clientes.ViewModelsLib.TransacaoNaHoraVM (TransacaoNaHora)
 
 type DBConnectionString = ByteString
 
-data TransacaoComSaldo = TransacaoComSaldo
-  { valor        :: Integer
-  , tipo         :: String
-  , descricao    :: String
-  , realizada_em :: UTCTime
-  , saldo        :: Integer
-  }
-pegarSaldoPelaTabela :: [TransacaoComSaldo]
-
-obterExtrato :: Integer -> Handler ExtratoNaHora
-obterExtrato idCliente = fmap (map fromOnly) . liftIO $
-                           withResource conns $ \conn ->
-                             query conn
-                               "SELECT valor, tipo, descricao, realizada_em, saldo" ++
-                               " FROM Transacoes" ++
-                               " WHERE id_cliente = ?" ++
-                               " ORDER BY realizada_em DESC" ++
-                               " LIMIT 10"
-                               ( idCliente )
+obterExtrato :: Pool Connection -> Integer -> Handler ExtratoNaHora
+obterExtrato conns idCliente = do
+  transacoes :: [TransacaoNaHora]
+  transacoes <- fmap (map fromOnly) . liftIO $
+    withResource conns $ \conn ->
+      query conn
+        "SELECT valor, tipo, descricao, realizada_em" ++
+        " FROM Transacoes WHERE id_cliente = ?" ++
+        " ORDER BY realizada_em DESC" ++
+        " LIMIT 10;"
+        ( idCliente )
+  saldos :: [SaldoNaHora]
+  saldos <- fmap (map fromOnly) . liftIO $
+    withResrouce conns $ \conn ->
+      query conn
+        "SELECT [total] = saldo, limite, data_extrato = NOW()" ++
+        " FROM Clientes WHERE id_cliente = ?"
+        ( idCliente )
+  case saldos of
+    []        -> throwError (err404 ())
+    [saldo:_] -> return ExtratoNaHora { saldo , ultimas_transacoes = transacoes }
 
