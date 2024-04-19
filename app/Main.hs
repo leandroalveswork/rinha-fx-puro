c79a0ef1a276b435856b0d4b073112df0b2a8cb9
leandroalveswork <leandroprogramadoruxdesign@gmail.com>
2024-04-18 22:32:23 -0300

    mudar o Main, e usar temporariamente string chumbada

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Network.Wai.Handler.Warp
 import Servant
 
 import Clientes.ApiLib.Api           (ClientesApi, clientesServidor)
+import RepositoryLib.Repository      (migrateDB, initConnectionPool)
 
 proxyServidor :: Proxy ClientesApi
 proxyServidor = Proxy
@@ -13,4 +14,7 @@ appRinha :: Application
 appRinha = serve proxyServidor clientesServidor
 
 main :: IO ()
-main = run 9999 appRinha
+main = do
+  pool <- initConnectionPool "Host=localhost;Port=5432;Database=rinhafx;User ID=root;Password=1234"
+  migrateDB pool
+  run 9999 (serve proxyServidor $ clientesServidor pool)
