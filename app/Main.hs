3a89a6c59416b734148d9564ba9c20c1bc449729
leandroalveswork <leandroprogramadoruxdesign@gmail.com>
2024-04-20 22:30:01 -0300

    adequar na Main.hs e dependencias

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,26 @@
 module Main where
 
+import Data.ByteString             (ByteString)
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
-
-import Clientes.ApiLib.Api           (ClientesApi, clientesServidor)
-import RepositoryLib.Repository      (migrateDB, initConnectionPool)
+import Data.Pool
+import Database.PostgreSQL.Simple
+import Clientes.ApiLib.Api         (ClientesApi, clientesServidor)
+import RepositoryLib.Repository    (migrateDB, initConnectionPool, DBConnectionString)
+import Data.String (IsString(fromString))
 
 proxyServidor :: Proxy ClientesApi
 proxyServidor = Proxy
 
-appRinha :: Application
-appRinha = serve proxyServidor clientesServidor
+appRinha :: Pool Connection -> Application
+appRinha conns = serve proxyServidor (clientesServidor conns)
+
+connstr :: DBConnectionString
+connstr = fromString "Host=localhost;Port=5432;Database=rinhafx;User ID=root;Password=1234"
 
 main :: IO ()
 main = do
-  pool <- initConnectionPool "Host=localhost;Port=5432;Database=rinhafx;User ID=root;Password=1234"
-  migrateDB pool
+  pool <- initConnectionPool connstr
+  migrateDB connstr
   run 9999 (serve proxyServidor $ clientesServidor pool)
